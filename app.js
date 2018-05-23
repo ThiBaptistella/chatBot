d96a6465b0dd32ce108cf8b9193e610937ff5828
thiago baptistella <thiago_baptistella1@hotmail.com>
2018-05-23 22:12:59 +0100

    add missing file

---
+++ app.js
@@ -0,0 +1,18 @@
+
+{
+  "name": "Web Speech AI Chatbot guide for Brazilian foreign",
+  "description": "Simple AI Chat Bot Demo with Web Speech API",
+  "repository": "https://github.com/ThiBaptistella/chatBot.git",
+  "keywords": ["ai", "api.ai", "bot", "w3c"],
+  "env": {
+    "APIAI_TOKEN": {
+      "description": "API.ai Client access token",
+      "value": ""
+    },
+    "APIAI_SESSION_ID": {
+      "description": "An arbitrary string",
+      "value": ""
+    }
+  },
+  "image": "heroku/nodejs"
+}
